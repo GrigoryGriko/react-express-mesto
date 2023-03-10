a790113c2d6ef060ed71d9b7aea0c77c1ac5e56e
Grigory Griko <griko1996@gmail.com>
2023-03-10 11:34:19 +0300

    Добавляет пакет CORS

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const { cors } = require('cors');
 const routes = require('./routes/index');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
