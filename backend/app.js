9127969f91d686908f519fb7e7ff448e55888605
Grigory Griko <griko1996@gmail.com>
2023-03-12 17:18:09 +0300

    Исправлена проблема с CORS

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const { cors } = require('cors');
+const cors = require('cors');
 const routes = require('./routes/index');
 
 const { PORT = 3000 } = process.env;
