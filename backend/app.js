6c280b696dff09d2f6090c046ed26be97da2add4
Grigory Griko <griko1996@gmail.com>
2023-03-13 09:38:21 +0300

    Добавлено логгирование запросов

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const { errors } = require('celebrate');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const routes = require('./routes/index');
+const NotFoundError = require('./errors/NotFoundError');
+const errorHandler = require('./middlewares/errorHandler');
 
 const { PORT = 3000 } = process.env;
 
@@ -13,7 +17,17 @@ app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
+app.use(requestLogger);
+
 app.use(routes);
+
+app.use(errorLogger);
+
+app.use((req, res, next) => {
+  next(new NotFoundError('Данный ресурс не найден'));
+});
+app.use(errors());
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Сервер работает (порт: ${PORT})`);
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,12 +1,9 @@
 const router = require('express').Router();
-const { errors } = require('celebrate');
 const cookieParser = require('cookie-parser');
 
-const errorHandler = require('../middlewares/errorHandler');
 const usersRouter = require('./usersRouter');
 const cardsRouter = require('./cardsRouter');
 const auth = require('../middlewares/auth');
-const NotFoundError = require('../errors/NotFoundError');
 const {
   validateUserLogin,
   validateUserRegister,
@@ -25,11 +22,5 @@ router.use('/cards', cardsRouter);
 router.get('/signout', (req, res) => {
   res.clearCookie('jwt').send({ message: 'Выход' });
 });
-
-router.use((req, res, next) => {
-  next(new NotFoundError('Данный ресурс не найден'));
-});
-router.use(errors());
-router.use(errorHandler);
 
 module.exports = router;
